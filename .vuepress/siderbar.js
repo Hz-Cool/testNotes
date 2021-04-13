724fdaf5630aaa84540403687634c2e8ec575c2c
Ziwei.Hu <ziwei.hu@tarsocial.com>
2021-04-13 14:36:49 +0800

    first post

--- .vuepress/siderbar.js
+++
@@ -1,9 +0,0 @@
-const { createSideBarConfig } = require('./util')
-const CSS_PATH = '/blogs/css'
-const React_PATH = '/blogs/react'
-
-
-module.exports = {
-  [CSS_PATH]: [createSideBarConfig('CSS 技巧', CSS_PATH)],
-  [React_PATH]: [createSideBarConfig('React 技巧', React_PATH)],
-}
